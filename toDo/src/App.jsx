dfe645e696db8c2875d8e06d17a5699a2f2172a9
Mert <mertkacar5800@gmail.com>
2023-06-08 01:25:34 +0300

    toDoListV2(JsonServer)
    UseEffect and json server added.

--- toDo/src/App.jsx
+++ toDo/src/App.jsx
@@ -1,49 +1,66 @@
-import { useState } from 'react'
-import './App.css'
-import TaskCreate from './components/TaskCreate'
-import TaskList from './components/TaskList'
- 
-function App() {
- //? UseSnappet oluşturma  Array'e atayacağız.
- const [tasks, setTasks] = useState([]);
- 
- //? onClick verileri Aktarma.
- const createTask = (title,taskDesc) =>{
-  const createdTasks = [
-      ...tasks,
-      {
-         id:Math.round(Math.random()*999999),
-         title:title,
-         taskDesc:taskDesc,
-      },
-   ];
-    setTasks(createdTasks);
- }
+import { useEffect, useState } from "react";
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
+import axios from "axios";
 
-   const deleteTaskById = (id)=>{
-    const afterDeletingTasks =  tasks.filter((task) =>{
-         return  task.id !== id;
-      })
-      setTasks(afterDeletingTasks)
-   }
-   
-  const editTaskById = (id,updatedTitle,updatedTaskDesc) =>{
-      const updatedTasks = tasks.map((task)=>{
-            if(task.id == id)
-            {
-               return {id:id,title:updatedTitle,taskDesc:updatedTaskDesc}
-            }
-            return task;
-      });
-      setTasks(updatedTasks);
+function App() {
+  //? UseSnappet oluşturma  Array'e atayacağız.
+  const [tasks, setTasks] = useState([]);
+
+  //? onClick verileri Aktarma.
+  const createTask = async (title, taskDesc) => {
+    const respone = await axios.post("http://localhost:3000/tasks", {
+      title,
+      taskDesc,
+    });
+    console.log(respone);
+
+    const createdTasks = [...tasks, respone.data];
+    setTasks(createdTasks);
+  };
+  const fetchTasks = async () => {
+    const response = await axios.get("http://localhost:3000/tasks");
+      
+    setTasks(response.data);
+  };
+  
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3000/tasks/${id}`); //!YAMUK TIRNAK KULLANILIR.
+    const afterDeletingTasks =  await tasks.filter((task) => {
+      return task.id !== id;
+    });
+    setTasks(afterDeletingTasks);
+  };
+
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3000/tasks/${id}`,{
+      title:updatedTitle,
+      taskDesc:updatedTaskDesc,
+    });
+    const updatedTasks = tasks.map((task) => {
+      if (task.id == id) {
+        return { id: id, title: updatedTitle, taskDesc: updatedTaskDesc };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   };
   return (
-     <div className='App'>
-        <TaskCreate onClick={createTask} onChange ></TaskCreate>
-        <h1>Görevler</h1>
-        <TaskList tasks={tasks}  onDelete={deleteTaskById} onUpdate={editTaskById} ></TaskList>
-     </div>
-  )
+    <div className="App">
+      <TaskCreate onClick={createTask} onChange></TaskCreate>
+      <h1>Görevler</h1>
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onUpdate={editTaskById}
+      ></TaskList>
+    </div>
+  );
 }
 
-export default App
+export default App;
